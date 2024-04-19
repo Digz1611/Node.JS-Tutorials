39c31e1b8eb423d5e715a0ecdb352c39cb87247e
Digz1611 <diego.langeveldt@gmail.com>
2024-04-19 13:13:09 +0200

    mongodb sorted

--- chapter_5/test.js
+++ chapter_5/test.js
@@ -1,20 +1,13 @@
 const mongoose = require('mongoose');
 const BlogPost = require('./models/BlogPost');
 // mongoose.connect('mongodb://localhost:27017/my_database' , { useFindAndModify: false, useCreateIndex: true });
-mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/my_database', {useNewUrlParser: true , useUnifiedTopology: true });
 
 BlogPost.create({
     title: "The Mythbuster's Guide to Vaccines",
     body: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis justo nec mauris aliquam ultricies. Aliquam erat volutpat. Etiam bibendum augue elementum ante aliquet, et semper mauris laoreet. In odio mi, feugiat auctor egestas eu, luctus vitae ligula. Pellentesque libero sapien, tempor nec hendrerit non, porta ut erat. Nunc porta suscipit odio, eu tincidunt nisi congue in. Etiam sit amet ullamcorper velit, vitae efficitur mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi vitae luctus risus. Cras nec metus bibendum, pharetra nulla et, lobortis ex. Fusce dapibus rhoncus semper. Proin lacus lacus, sagittis eget est nec, scelerisque porta eros. Curabitur tempus ac neque et consequat. Mauris commodo hendrerit metus, at tempus orci dignissim id. Curabitur sagittis, mauris sed commodo tristique, ligula ipsum molestie ligula, eu mollis lectus dolor in est. In quis elementum ante. Curabitur tortor nulla, rutrum id justo nec, vulputate porttitor nisl. Aliquam feugiat ex aliquet ante euismod malesuada. Curabitur eu faucibus magna. Nulla placerat ipsum magna, id sollicitudin sem venenatis vulputate. In id felis sed risus gravida posuere. Donec viverra imperdiet congue. Sed pulvinar metus at augue volutpat, vel feugiat nunc bibendum. Quisque dignissim risus sed nisi porta imperdiet. Mauris pellentesque diam sed fringilla mattis. Nunc sed pellentesque dolor. Etiam posuere bibendum magna nec vulputate. Sed ac feugiat arcu."
 }, (error, blogpost) => {
     console.log(error, blogpost);
-});
-
-BlogPost.create({
-    title: 'The Mythbuster Guide to Saving Money on Energy Bills',
-    body: 'If you have been here a long time, you might remember when I went on ITV Tonight to dispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics, because once you get past the boring bullet-point lists, a whole new world of thrifty nerdery opens up. You know those bullet-point lists. You start spotting them everything at this time of year. They go like this:'
-}, (error, blogpost) => {
-    console.log(error, blogpost)
 });
 
 /* BlogPost.find({}, (error, blogpost) => {
@@ -39,6 +32,6 @@ BlogPost.create({
     console.log(error, blogpost)
 }); */
 
-BlogPost.findByIdAndDelete(id, (error, blogpost) => {
-    console.log(error, blogpost)
-});
+// BlogPost.findByIdAndDelete(id, (error, blogpost) => {
+//     console.log(error, blogpost)
+// });
